5db982ee72912e42d5ba2f5765d25a610e13902a
tuanphampp9 <tuanphampp9@gmail.com>
2026-03-25 15:09:15 +0700

    adjust version build android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 18
-        versionName = "2.1.0"
+        versionCode = 19
+        versionName = "2.1.1"
         
         // Android 15 specific configurations
         ndk {
